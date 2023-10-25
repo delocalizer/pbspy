f3cff9565cd8d5b6f1bffcb3d7dee6002308e8f5
delocalizer <conrad.leonard@hotmail.com>
2023-10-25 13:19:12 +1000

    first

--- pbs.py
+++ pbs.py
@@ -15,34 +15,39 @@ import asyncio
 from asyncio.subprocess import PIPE
 from dataclasses import dataclass
 from os import getcwd, path
-from typing import Any, Dict, Optional
+from sys import stderr
+from typing import Any, Dict, Optional, Sequence
 
 from returns.future import future_safe
+from returns.io import IOFailure, IOResult, IOSuccess
 from returns.pipeline import flow
 from returns.pointfree import bind
+from returns.result import Success
 
 
 @dataclass
 class JobSpec:
     """Specifies a PBS batch job."""
+
     cmd: str
     name: str
     ask_mem: str
     ask_ncpus: int
     ask_walltime: str
-    path_err: Optional[str]=None
-    path_out: Optional[str]=None
+    path_err: Optional[str] = None
+    path_out: Optional[str] = None
 
 
 @dataclass
 class Job:
     """Describes a submitted PBS job."""
-    job_id: int 
+
+    job_id: int
     err_path: str
 
 
 @future_safe
-async def submit(jobspec: Optional[JobSpec|Dict[str, Any]]=None, **kwargs) -> Job:
+async def submit(jobspec: Optional[JobSpec | Dict[str, Any]] = None, **kwargs) -> Job:
     """Submit a job to the scheduler."""
     match jobspec:
         case JobSpec():
@@ -52,50 +57,68 @@ async def submit(jobspec: Optional[JobSpec|Dict[str, Any]]=None, **kwargs) -> Jo
         case _:
             jobspec = JobSpec(**kwargs)
 
-    proc = await asyncio.create_subprocess_shell(f'''
+    proc = await asyncio.create_subprocess_shell(
+        f'''
         qsub \\
         -l mem={jobspec.ask_mem} \\
         -l ncpus={jobspec.ask_ncpus} \\
         -l walltime={jobspec.ask_walltime} \\
         -N "{jobspec.name}"''',
-        stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = await proc.communicate(input=jobspec.cmd.encode('utf-8'))
+        stdin=PIPE,
+        stdout=PIPE,
+        stderr=PIPE,
+    )
+    stdout, stderr = await proc.communicate(input=jobspec.cmd.encode("utf-8"))
     if proc.returncode != 0:
         raise Exception(f'{jobspec}: {stderr.decode("utf-8")}')
-    job_id = int(stdout.decode('utf-8').split('.')[0].strip())
-    job_name = jobspec.name or 'STDIN'
-    err_path = jobspec.path_err or f'{getcwd()}/{job_name}.e{job_id}'
+    job_id = int(stdout.decode("utf-8").split(".")[0].strip())
+    job_name = jobspec.name or "STDIN"
+    err_path = jobspec.path_err or f"{getcwd()}/{job_name}.e{job_id}"
     return Job(job_id, err_path)
 
 
 @future_safe
-async def wait_till_done(job: Job, waitsec=10) -> bool:
+async def wait_till_done(job: Job, waitsec=10) -> Job:
     """Wait until the job is finished."""
     while True:
-        print(f'checking {job.job_id}...')
         if path.exists(job.err_path):
-            print(f'{job.job_id} done!')
-            return True
+            # TODO: single qstat call to check job exit status
+            return job 
         await asyncio.sleep(waitsec)
 
 
-async def main():
+def handle_results(results: Sequence[IOResult[Job, Exception]]) -> None:
+    """Do something with accumulated results."""
+    for result in results:
+        match result:
+            case IOFailure(ex):
+                print(ex, file=stderr)
+            case IOSuccess(Success(job)):
+                print(f'{job.job_id} completed')
 
+
+async def main():
     jobs = [
-        JobSpec(cmd='echo foo',
-                name='a-sitting-on-a-gate',
-                ask_mem='1MB',
-                ask_ncpus=1,
-                ask_walltime='00:01:00'),
-        JobSpec(cmd='echo bar',
-                name='haddocks-eyes',
-                ask_mem='1MB',
-                ask_ncpus=1,
-                ask_walltime='00:01:00'),
+        JobSpec(
+            cmd="echo foo",
+            name="a sitting-on-a-gate",
+            ask_mem="1MB",
+            ask_ncpus=1,
+            ask_walltime="00:01:00",
+        ),
+        JobSpec(
+            cmd="echo bar",
+            name="haddocks-eyes",
+            ask_mem="1MB",
+            ask_ncpus=1,
+            ask_walltime="00:01:00",
+        ),
     ]
 
-    await asyncio.gather(*(flow(job, submit, bind(wait_till_done)) for job in jobs))
-    
+    # TODO: Semaphore to throttle number of job submissions and checks
+    flows = [flow(job, submit, bind(wait_till_done)) for job in jobs]
+    handle_results(await asyncio.gather(*flows))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
